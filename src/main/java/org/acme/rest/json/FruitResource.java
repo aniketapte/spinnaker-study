8bc978956b139a16903f498e33e871976583ea29
aniketapte <aniket.apte@gmail.com>
2020-11-09 14:59:04 -0500

    Update FruitResource.java

--- src/main/java/org/acme/rest/json/FruitResource.java
+++ src/main/java/org/acme/rest/json/FruitResource.java
@@ -22,6 +22,7 @@ public class FruitResource {
     public FruitResource() {
         fruits.add(new Fruit("Apple", "Winter fruit"));
         fruits.add(new Fruit("Pineapple", "Tropical fruit"));
+        fruits.add(new Fruit("Mango", "Alphonso is the best"));
     }
 
     @GET
